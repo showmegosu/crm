fca956004183172330e16b8c1f0f642b5ef660bd
Oleksandr Gryn <oleksandr.gryn@synapse.com>
2018-05-02 16:48:17 +0300

    Adding Client class and its repository.

---
+++ Tourism.DAL/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tourism.DAL
+{
+    public class Client
+    {
+        public long Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string FathersName { get; set; }
+        public List<object> PhoneNumbers { get; set; }
+        public List<object> Addresses { get; set; }
+
+        public Client(long id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ Tourism.DAL/ClientRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Tourism.DAL
+{
+    public class ClientRepository
+    {
+        public List<Client> GetAllClients()
+        {
+            var clientsList = new List<Client>();
+
+            var connectionString = "Data Source=(local);Initial Catalog=CRM;" + "Integrated Security=true";
+
+            var queryString = "SELECT Client_Id, Surname from dbo.Clients";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand(queryString, connection);
+
+                try
+                {
+                    connection.Open();
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        clientsList.Add(new Client((int)reader[0], reader[1].ToString()));
+                    }
+
+                    reader.Close();
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.ReadLine();
+            }
+
+            return clientsList;
+        }
+    }
+}
