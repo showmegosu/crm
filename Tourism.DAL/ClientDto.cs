6ada91af4d62d599a27ba79b4ebc5a8e26afd344
Nacho <nacho.jorge@gmail.com>
2018-05-03 23:55:04 +0300

    Added back the constructor

--- Tourism.DAL/ClientDto.cs
+++ Tourism.DAL/ClientDto.cs
@@ -2,6 +2,16 @@
 {
     public class ClientDto
     {
+        public ClientDto()
+        {
+        }
+
+        public ClientDto(int id, string surname)
+        {
+            Id = id;
+            Surname = surname;
+        }
+
         public int Id { get; set; }
         public string Surname { get; set; }
     }
