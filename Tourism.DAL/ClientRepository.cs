9ea10a974a8628327386f650c6d6dc214623c5cd
Nacho <nacho.jorge@gmail.com>
2018-05-22 15:24:37 +0300

    Fixed the connectionString

--- Tourism.DAL/ClientRepository.cs
+++ Tourism.DAL/ClientRepository.cs
@@ -1,22 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 
 namespace Tourism.DAL
 {
     public class ClientRepository
     {
+        private readonly string _connectionString;
+
         public ClientRepository(string connectionString)
         {
-            this.ConnectionString = connectionString;
+            if (connectionString == null) throw new ArgumentNullException(nameof(connectionString));
+            _connectionString = connectionString;
         }
-        private string ConnectionString { get; }
         public List<ClientDto> GetAllClients()
         {
             var clientsList = new List<ClientDto>();
 
             var queryString = "SELECT Client_Id, Surname from dbo.Clients";
 
-            using (var connection = new SqlConnection(ConnectionString))
+            using (var connection = new SqlConnection(_connectionString))
             {
                 var command = new SqlCommand(queryString, connection);
                 connection.Open();
