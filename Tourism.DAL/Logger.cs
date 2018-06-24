d68c2a414aede3db8208c74dfb658d3f5fa4196c
showmegosu <admin@shopmall.com>
2018-06-24 22:52:01 +0300

    Initial commit.

---
+++ Tourism.DAL/Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tourism.DAL
+{
+    public sealed class Logger
+    {
+        private StreamWriter writer;
+        private Logger()
+        {
+            string fileName = DateTime.Now.ToString("YYYYMMDD");
+            writer = new StreamWriter("date.txt");
+        }
+
+        public static Logger Instance => Nested.Instance;
+
+        private class Nested
+        {
+            static Nested()
+            {
+            }
+
+            internal static readonly Logger Instance = new Logger();
+        }
+
+        public void LogException()
+        {
+        }
+
+        public void LogWarning()
+        {
+        }
+
+        public void LogInfo()
+        {
+        }
+    }
+}
