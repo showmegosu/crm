feddcf4477c6df5bf0ea8cdc050cda309411f657
showmegosu <admin@shopmall.com>
2018-07-07 23:38:35 +0300

    Adding GlobalExceptionHandling project and fixing Logger class.

---
+++ GlobalExceptionHandling/SetupFiltersExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using Owin;
+
+namespace GlobalExceptionHandling
+{
+    public static class SetupFiltersExtensions
+    {
+        public static IAppBuilder SetupFilters(this IAppBuilder builder, HttpConfiguration config)
+        {
+            config.Services.Replace(typeof(IExceptionHandler), new GlobalExceptionHandler());
+            config.Services.Add(typeof(IExceptionLogger), new Logger.Logger("D://"));
+
+            return builder;
+        }
+    }
+}
