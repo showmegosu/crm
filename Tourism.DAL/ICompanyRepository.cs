65e341aef66828dee3cac4444fc40f9488b4f99d
Oleksandr Gryn <oleksandr.gryn@synapse.com>
2018-09-21 20:21:49 +0300

    Adding company and office interfaces

---
+++ Tourism.DAL/ICompanyRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Tourism.DAL
+{
+    public interface ICompanyRepository
+    {
+        List<Company> GetAllCompanies();
+        Company GetCompanyById(int id);
+        int InsertCompany(Company company);
+        void Update(Company company);
+        void Delete(int id);
+    }
+}
--- Tourism.DAL/Office.cs
+++ Tourism.DAL/Office.cs
@@ -5,8 +5,20 @@ namespace Tourism.DAL
     public class Office
     {
         public int Id { get; set; }
+        public string Name { get; set; }
         public string Address { get; set; }
         public List<string> PhoneNumbers { get; set; }
-        public Company Company { get; set; }
+        public int Company { get; set; }
+
+        public Office(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Office()
+        {
+            PhoneNumbers = new List<string>();
+        }
     }
 }
